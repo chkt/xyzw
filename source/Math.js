16a80855fe96c0462e5f7abc8957d00bb8d99fe1
chkt <ck@christoph-kettelhoit.de>
2016-12-17 22:49:24 +0800

    Update type hints

--- source/Math.js
+++ source/Math.js
@@ -7,10 +7,10 @@ const ExtMath = Object.create(Math);
 
 /**
  * Returns the clamped value of n
- * @param {Number} n - The value
- * @param {Number} min - The minimal value
- * @param {Number} max - The maximal value
- * @returns {Number}
+ * @param {number} n - The value
+ * @param {number} min - The minimal value
+ * @param {number} max - The maximal value
+ * @returns {number}
  */
 ExtMath.clamp = function(n, min, max) {
 	return n < min ? min : (n > max ? max : n);
@@ -18,10 +18,10 @@ ExtMath.clamp = function(n, min, max) {
 
 /**
  * Returns the linear interpolation of a and b
- * @param {Number} a - The first value
- * @param {Number} b - The second value
- * @param {Number} bf - The weight of the second value
- * @returns {Number}
+ * @param {number} a - The first value
+ * @param {number} b - The second value
+ * @param {number} bf - The weight of the second value
+ * @returns {number}
  */
 ExtMath.mix = function(a, b, bf) {
 	return a * (1.0 - bf) + b * bf;
@@ -29,9 +29,9 @@ ExtMath.mix = function(a, b, bf) {
 
 /**
  * Returns the reflected value of n against r
- * @param {Number} n - The value
- * @param {Number} r - The reflection value
- * @returns {Number}
+ * @param {number} n - The value
+ * @param {number} r - The reflection value
+ * @returns {number}
  */
 ExtMath.reflect = function(n, r) {
 	return 2.0 * r - n;
@@ -39,10 +39,10 @@ ExtMath.reflect = function(n, r) {
 
 /**
  * Returns a random number between min and max
- * @param {Number} min - The minimal value
- * @param {Number} max - The maximal value
- * @param {Int} [intervals] - The number of discreet intervals
- * @returns {Number}
+ * @param {number} min - The minimal value
+ * @param {number} max - The maximal value
+ * @param {int} [intervals] - The number of discreet intervals
+ * @returns {number}
  */
 ExtMath.range = function(min, max, intervals) {
 	const n = Math.random();
@@ -54,11 +54,11 @@ ExtMath.range = function(min, max, intervals) {
 
 /**
  * Returns true if ranges (a0 a1) and (b0 b1) overlap, false otherwise
- * @param {Number} a0 - The first limit of range a
- * @param {Number} a1 - The second limit of range a
- * @param {Number} b0 - The first limit of range b
- * @param {Number} b1 - The second limit of range b
- * @returns {Boolean}
+ * @param {number} a0 - The first limit of range a
+ * @param {number} a1 - The second limit of range a
+ * @param {number} b0 - The first limit of range b
+ * @param {number} b1 - The second limit of range b
+ * @returns {boolean}
  */
 ExtMath.overlap = function(a0, a1, b0, b1) {
 	[a0, a1] = a0 < a1 ? [a0, a1] : [a1, a0];
